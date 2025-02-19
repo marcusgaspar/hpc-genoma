f9464c1402f4d9d4982d83d67a77585741f20fdb
Marcus Gaspar <marcusga@microsoft.com>
2025-02-19 19:16:52 -0300

    Include low_gen, medium_gen and high_gen node arrays

--- import-template.sh
+++ import-template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-template_name="Slurm-Genoma4"
+template_name="Slurm-Genoma5"
 template_file="/anf-vol1/genomica/hpc-genoma/genomica-proj/templates/slurm-genoma-template-login-node.txt"
 
 echo "Importing template $template_name from $template_file"
