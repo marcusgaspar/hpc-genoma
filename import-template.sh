8cd712dafb1fc0752837a7c66526f4edf9d94fa1
Marcus Gaspar <marcusga@microsoft.com>
2025-05-05 11:49:27 -0300

    adjusting login node settings

--- import-template.sh
+++ import-template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-template_name="Slurm-Genoma5"
-template_file="/anf-vol1/genomica/hpc-genoma/genomica-proj/templates/slurm-genoma-template-login-node.txt"
+template_name="Slurm-Genoma-ITV"
+template_file="/anf-vol1/genomica/hpc-genoma/genomica-proj/templates/slurm-genoma-template-login-node-itv.txt"
 
 echo "Importing template $template_name from $template_file"
 
